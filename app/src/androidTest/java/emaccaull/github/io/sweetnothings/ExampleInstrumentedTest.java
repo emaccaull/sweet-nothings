ebf0a95ef08ae8b0832ddca8fa181ccf4f4603f2
Emmanuel MacCaull <emmanuel.maccaull@gmail.com>
2018-08-28 22:38:49 -0700

    Reformat whitespace

--- app/src/androidTest/java/emaccaull/github/io/sweetnothings/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/emaccaull/github/io/sweetnothings/ExampleInstrumentedTest.java
@@ -17,11 +17,11 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
-  @Test
-  public void useAppContext() {
-    // Context of the app under test.
-    Context appContext = InstrumentationRegistry.getTargetContext();
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
 
-    assertEquals("emaccaull.github.io.sweetthings", appContext.getPackageName());
-  }
+        assertEquals("emaccaull.github.io.sweetthings", appContext.getPackageName());
+    }
 }
