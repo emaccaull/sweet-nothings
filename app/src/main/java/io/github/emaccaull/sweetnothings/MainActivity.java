70de68c531d2dfba6a1f1c5b2b07bb5449556607
Emmanuel MacCaull <emmanuel.maccaull@gmail.com>
2018-09-02 11:50:24 -0700

    Use Data Binding

--- app/src/main/java/io/github/emaccaull/sweetnothings/MainActivity.java
+++ app/src/main/java/io/github/emaccaull/sweetnothings/MainActivity.java
@@ -16,34 +16,34 @@
 
 package io.github.emaccaull.sweetnothings;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import io.github.emaccaull.sweetnothings.databinding.ActivityMainBinding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private Logger logger = LoggerFactory.getLogger(MainActivity.class);
+    private final Logger logger = LoggerFactory.getLogger(MainActivity.class);
+    private ActivityMainBinding binding;
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavigationListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    binding.message.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    binding.message.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    binding.message.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -54,11 +54,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         logger.info("+onCreate");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        mTextMessage = findViewById(R.id.message);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.navigation.setOnNavigationItemSelectedListener(bottomNavigationListener);
+        binding.navigation.setSelectedItemId(R.id.navigation_home);
+
         logger.info("-onCreate");
     }
 
